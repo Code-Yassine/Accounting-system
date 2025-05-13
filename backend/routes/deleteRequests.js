7aaef4d035ddbb0a36aebe13d3495664083f09d3
yassine.dev <mohamedyassinkoubi@gmail.com>
2025-05-13 13:26:53 +0100

    add authentication token to all api requests

--- backend/routes/deleteRequests.js
+++ backend/routes/deleteRequests.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const deleteRequestController = require('../controllers/deleteRequestController');
+const auth = require('../middleware/auth');
 
 // Create a new delete request
-router.post('/', deleteRequestController.createDeleteRequest);
+router.post('/', auth, deleteRequestController.createDeleteRequest);
 
 // Get all delete requests
-router.get('/', deleteRequestController.getDeleteRequests);
+router.get('/', auth, deleteRequestController.getDeleteRequests);
 
 // Update delete request status
-router.patch('/:id', deleteRequestController.updateDeleteRequestStatus);
+router.patch('/:id', auth, deleteRequestController.updateDeleteRequestStatus);
 
 module.exports = router;
